2a47ef97b71678e62eeafd1a839e2d2b1372e44c
aolatunji71@gmail.com <aolatunji71@gmail.com>
2025-11-05 09:06:32 +0100

    feat index.ts updated

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,8 +1,16 @@
 import { Mastra } from "@mastra/core/mastra";
 import { taskBuddy } from "./agents/task-buddy";
+import { a2aAgentRoute } from "./routes/a2a-agent-routes";
 
 export const mastra = new Mastra({
   agents: {
     taskBuddy,
+  },
+  server: {
+    build: {
+      openAPIDocs: true,
+      swaggerUI: true,
+    },
+    apiRoutes: [a2aAgentRoute]
   },
 });
